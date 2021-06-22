bb40ea7bbfd65561c971668b6ceb255a7ea4dcfa
gitgitWi <johnwi@knou.ac.kr>
2021-06-22 16:10:46 +0900

    Feat: NewsData Interface 정의

--- packages/wiii/domain/newsData.ts
+++ packages/wiii/domain/newsData.ts
@@ -1,3 +1,16 @@
+export interface NewsData {
+  id: number;
+  category?: string;
+  headline: string;
+  summary?: string;
+  datetime?: number;
+  image?: string;
+  source?: string;
+  url?: string;
+  likes?: number;
+  userLiked?: boolean;
+}
+
 export const enum marketEnum {
   general = 'general',
   crypto = 'crypto',
