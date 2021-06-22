00820beb7686a2a4101eda5171e90190f01f45f0
gitgitWi <johnwi@knou.ac.kr>
2021-06-22 16:21:56 +0900

    Feat: News 목록 API 호출시 북마크 관련 데이터 추가
    - 뉴스 Schema에서 북마크수 가져와서 추가
    - 사용자 로그인한 경우 사용자가 북마크한 뉴스 표시

--- packages/wiii/backend/service/News.service.ts
+++ packages/wiii/backend/service/News.service.ts
@@ -1,43 +1,82 @@
-import { getDateString, times, WEEK_ONE } from '../../domain/date';
+import { getCustomRepository } from 'typeorm';
 import { Service } from 'zum-portal-core/backend/decorator/Alias';
-import { getCompanyNews, getMarketNews } from './news';
-import { marketEnum } from '../../domain/newsData';
 import { Caching } from 'zum-portal-core/backend/decorator/Caching';
+import { getDateString, times, WEEK_ONE } from '../../domain/date';
+import { marketEnum, NewsData } from '../../domain/newsData';
+import { NewsRepository, UserRepository } from '../db';
+import { getCompanyNews, getMarketNews } from './news';
 import tickerMap from './tickerMap';
 
 @Service()
 export class NewsService {
-  /** @todo 개발 모드에서 300분 단위 캐싱 */
-  // private delay = MINUTE_ONE * 300;
-  // private lastRequest: number;
-  // private cachedNews = {};
-
-  constructor() {
-    // this.lastRequest = new Date().getTime() - MINUTE_ONE * 10;
-  }
+  constructor() {}
 
   /**
    * getMarketNewsList
    */
-  @Caching({
-    ttl: times.caching,
-    runOnStart: false,
-    refreshCron: '*/30 * * * *',
-  })
-  public getMarketNewsList(category: marketEnum) {
-    return getMarketNews(category);
+  // @Caching({
+  //   ttl: times.caching,
+  //   runOnStart: false,
+  //   refreshCron: '*/30 * * * *',
+  // })
+  public async getMarketNewsList(category: marketEnum, email?: string) {
+    const data = await getMarketNews(category);
+    return this.setNewsDataWithBookmark(data, email);
   }
 
   /**
    * getCompanyNewsList
    */
-  @Caching({
-    ttl: times.caching,
-    runOnStart: false,
-  })
-  public getCompanyNewsList(symbol: string, from?: string, to?: string) {
+  // @Caching({
+  //   ttl: times.caching,
+  //   runOnStart: false,
+  // })
+  public async getCompanyNewsList(symbol: string, from?: string, to?: string, email?: string): Promise<NewsData[]> {
     const now = new Date().getTime();
     if (symbol in tickerMap) symbol = tickerMap[symbol];
-    return getCompanyNews(symbol, from ?? getDateString(now - WEEK_ONE * 2), to ?? getDateString(now));
+
+    const data = await getCompanyNews(symbol, from ?? getDateString(now - WEEK_ONE * 2), to ?? getDateString(now));
+    return this.setNewsDataWithBookmark(data, email);
+  }
+
+  public createBookmarkNews(props: NewsData) {
+    return getCustomRepository(NewsRepository).insertNews(props);
+  }
+
+  /**
+   *
+   * @param data API로 받은 데이터 원본
+   * @param email 사용자 이메일
+   * @returns Bookmark likes/userLiked 추가된 데이터
+   */
+  private async setNewsDataWithBookmark(data: NewsData[], email?: string): Promise<NewsData[]> {
+    const newsIds = [];
+    /** newsId -> 인덱스 매핑, 북마크수 업데이트 위함 */
+    const newsIdMap = {};
+    data.forEach(({ id }, idx) => {
+      newsIds.push(id);
+      newsIdMap[id] = idx;
+    });
+
+    /** 북마크 갯수 추가 */
+    const markeds = await getCustomRepository(NewsRepository).selectNewsByIDs(newsIds);
+    // @ts-ignore
+    const markedIdMap = markeds.reduce((acc, cur, idx) => {
+      acc[cur.id] = idx;
+      return acc;
+    }, {});
+
+    data.forEach((d) => {
+      const { id } = d;
+      d.likes = id in markedIdMap ? markeds[markedIdMap[id]].likes : 0;
+    });
+
+    /** 로그인 정보 있는 경우 bookmark 데이터 가공 */
+    if (email || email !== '') {
+      const { bookmarkNews } = await getCustomRepository(UserRepository).findOne({ email });
+      data.forEach((d) => (d.userLiked = d.id in bookmarkNews));
+    }
+
+    return data;
   }
 }
